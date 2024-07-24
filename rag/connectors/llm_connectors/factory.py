ffe525694d1ffee9396cb5cf3a4a4cbe9662931b
oscar_leon <oscar.leon@klym.com>
2024-07-24 14:13:54 -0500

    clean files

--- rag/connectors/llm_connectors/factory.py
+++ rag/connectors/llm_connectors/factory.py
@@ -2,9 +2,12 @@ from rag.connectors.llm_connectors.openai_connector import OpenAILlmConnector
 from rag.connectors.llm_connectors.mistral_connector import MistralAiLlmConnector
 from rag.connectors.llm_connectors.enum import SuportedModels
 class LlmConnectorFactory:
+
     def get_connector(self, model_name):
 
-        models = {SuportedModels.openai_3_5_turbo:OpenAILlmConnector("gpt-3.5-turbo"), 
+        models = {
+        SuportedModels.openai_3_5_turbo:OpenAILlmConnector("gpt-3.5-turbo"), 
          SuportedModels.mistral:MistralAiLlmConnector(),
-         SuportedModels.openai_4:OpenAILlmConnector("gpt-4")}
+         SuportedModels.openai_4:OpenAILlmConnector("gpt-4")
+         }
         return models.get(model_name)
